3e51632cabe6372c476448c154273797e87cecc7
tchung52 <301287013@cencol.ca>
2022-10-25 12:58:35 -0400

    Changed database connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,21 @@
+var prefix = "mongodb+srv://";
+var dbUrl = "cluster0.jvedyi3.mongodb.net/";
+var postfix = "?retryWrites=true&w=majority";
+
+var dbName = "car_store";
+var username = "compuser";
+var password = "qNV4uYWrBdTELFdg";
+var fullUrl =
+  "mongodb+srv://<username>:<password>@cluster0.jvedyi3.mongodb.net/<dbname>?retryWrites=true&w=majority";
+//var connString = prefix+username+':'+password+'@'+dbUrl+dbName+postfix;
+fullUrl = fullUrl
+  .replace("<username>", username)
+  .replace("<password>", password)
+  .replace("<dbname>", dbName);
+
+
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/car_store",
+  //URI: "mongodb://127.0.0.1/car_store",
+  URI: fullUrl,
 };
