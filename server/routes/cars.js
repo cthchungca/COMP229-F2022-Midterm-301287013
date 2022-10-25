54a72cf920c14b02d48cbe7bd2edcfef5c0855f5
tchung52 <301287013@cencol.ca>
2022-10-25 13:26:40 -0400

    Implemented add and update function

--- server/routes/cars.js
+++ server/routes/cars.js
@@ -26,6 +26,9 @@ router.get("/add", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+   res.render("cars/add", {
+    title: "Add Car",
+  });
 });
 
 // POST process the Car  Details page and create a new Car  - CREATE
@@ -33,6 +36,26 @@ router.post("/add", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  /*
+    Step 1: Create a new object
+    Step 2: Commit to DB: Create
+  */
+  let newcar= car({
+    Carname: req.body.carname,
+    Category: req.body.category,
+    Carmodel: req.body.carmodel,
+    Price: req.body.price
+  });
+  car.create(newcar, (err, committedCar) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      console.log("Entry created.")
+      res.redirect("/cars");
+    }
+  });
+
 });
 
 // GET the Car Details page in order to edit an existing Car
@@ -40,6 +63,19 @@ router.get("/:id", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  /*
+    Step 1: Retrieve an Object by id: findOne(id)
+  */
+  targetId = req.params.id;
+  car.findById(targetId, (err, requestObj) => {
+    if(err)
+      console.log(err)
+    else
+      res.render("cars/details", {
+        title: "Edit" + requestObj.Carname,
+        cars: requestObj
+      });
+  });
 });
 
 // POST - process the information passed from the details form and update the document
@@ -47,6 +83,25 @@ router.post("/:id", (req, res, next) => {
   /*****************
    * ADD CODE HERE *
    *****************/
+  /*
+    Step 1: Create new Object with id
+    Step 2: Commit to DB: updateOne
+  */
+  targetId = req.params.id;
+  let updatedRecord = car({
+    _id: targetId,
+    Carname: req.body.Carname,
+    Category: req.body.Category,
+    Carmodel: req.body.Carmodel,
+    Price: req.body.Price
+  });
+
+  car.updateOne({_id: targetId}, updatedRecord, (err) =>{
+    if(err)
+      console.log(err);
+    else
+      res.redirect("/cars");
+  });
 });
 
 // GET - process the delete
